db0a1b126a7ac9b31d07f58426480e94afc3f4f4
fdfast5 <fdfast5@gmail.com>
2023-05-16 14:12:39 +0900

    userテーブル用マイグレーション処理作成

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id         = models.BigAutoField
+    twitch_user_id  = models.CharField(max_length=10)
+    user_login      = models.CharField(max_length=256)
+    user_name       = models.CharField(max_length=256, null=True)
+    valid_flag      = models.BooleanField(default=True)
+    created_at      = models.DateTimeField()
+    updated_at      = models.DateTimeField()
+
+    class Meta:
+        db_table = 'user'
