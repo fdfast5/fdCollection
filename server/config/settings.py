d24f8fe775611063e6ee9e0de71b4813a4b4dc07
fdfast5 <fdfast5@gmail.com>
2023-05-16 13:05:04 +0900

    DBをMySQLに変更

--- server/config/settings.py
+++ server/config/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fd_collection',
+        'USER': 'fd',
+        'PASSWORD': 'fdfast',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
