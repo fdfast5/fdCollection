dd7bdf94bc568dfdc2a9798e09902d5de574997b
fdfast5 <fdfast5@gmail.com>
2023-05-31 17:15:00 +0900

    API分岐

--- server/api/twitch_api.py
+++ server/api/twitch_api.py
@@ -6,23 +6,25 @@ import environ
 env = environ.Env()
 env.read_env('.env')
 
-async def twitch_example(access_token):
+async def twitch_example(access_token, select_api):
     app_id = env('TWITCH_APP_ID')
     app_secret = env('TWITCH_APP_SECRET')
     #Twitch developersで取得したクライアントIDとシークレットキーを入力する
     twitch = await Twitch(app_id, app_secret)
     await twitch.set_user_authentication(access_token, [], access_token)
-    #自身のTwitchユーザー名を入力
-    #ここは表示名ではないことに注意
-    user = await first(twitch.get_users())
+
+    if select_api == 'user':
+        result = await first(twitch.get_users())
+    elif select_api == 'reward':
+        result = await twtich.get_custom_reward()
     #ユーザー情報を取得する
     await twitch.close()
-    return user
+    return result
 
-async def main(access_token):
-    coro = twitch_example(access_token)
+async def main(access_token, select_api):
+    coro = twitch_example(access_token, select_api)
     value = await coro
     return value
 
-def twitch_return(access_token):
-    return asyncio.run(main(access_token))
+def twitch_return(access_token, select_api):
+    return asyncio.run(main(access_token, select_api))
