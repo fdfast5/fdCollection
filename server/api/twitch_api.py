88d7ce162a58de7edfb9d8edcfc16568434a749e
fdfast5 <fdfast5@gmail.com>
2023-06-14 17:10:25 +0900

    env取得修正

--- server/api/twitch_api.py
+++ server/api/twitch_api.py
@@ -1,14 +1,11 @@
 from twitchAPI.twitch import Twitch
 from twitchAPI.helper import first
 import asyncio
-import environ
-
-env = environ.Env()
-env.read_env('.env')
+import os
 
 async def twitch_example(access_token, select_api, user_id):
-    app_id = env('TWITCH_APP_ID')
-    app_secret = env('TWITCH_APP_SECRET')
+    app_id = os.getenv('TWITCH_APP_ID')
+    app_secret = os.getenv('TWITCH_APP_SECRET')
     #Twitch developersで取得したクライアントIDとシークレットキーを入力する
     twitch = await Twitch(app_id, app_secret)
     await twitch.set_user_authentication(access_token, [], access_token)
@@ -16,7 +13,7 @@ async def twitch_example(access_token, select_api, user_id):
     if select_api == 'user':
         result = await first(twitch.get_users())
     elif select_api == 'reward':
-        result = await twitch.get_custom_reward(user_id)
+        result = await twitch.get_custom_reward(str(user_id))
     #ユーザー情報を取得する
     await twitch.close()
     return result
